6e06773e86f84db45fa9389829351e65874a528f
Vincent-glitch <66323103+Vincent-glitch@users.noreply.github.com>
2020-12-04 15:00:34 -0500

    cleaner

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Worked with a company's microtransaction data to generate a report that breaks down the game's purchasing data into meaningful insights though an analysis that includes product profitability, purchasing habits, demographic breakdowns for age and gender, and total revenues.',
+    author='Vincent D'Alto',
+    license='MIT',
+)
